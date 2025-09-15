e66d44b735cebb7d20abfec9ab45da1959178368
zen96k <jolly96k@gmail.com>
2025-09-16 03:18:11 +0900

    PostgreSQLを構築

--- .devcontainer/script/post_attach_command.sh
+++ .devcontainer/script/post_attach_command.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-docker container exec -it ollama ollama pull qwen2.5-coder
+docker container exec -it ollama ollama pull qwen3-coder
 cp -rfv config.yaml ${HOME}/.continue
 
 fastfetch
