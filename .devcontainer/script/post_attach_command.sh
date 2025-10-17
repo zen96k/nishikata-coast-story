808fe258fcae5982efed8337240691b52b4121ca
zen96k <jolly96k@gmail.com>
2025-10-17 19:51:41 +0900

    GitHub Actionsをテスト

--- .devcontainer/script/post_attach_command.sh
+++ .devcontainer/script/post_attach_command.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-docker container exec -it ollama ollama pull gemma3
+docker container exec ollama ollama pull gemma3
 cp -rfv config.yaml ${HOME}/.continue
