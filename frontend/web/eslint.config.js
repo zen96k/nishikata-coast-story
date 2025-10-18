f463b48027020db792d05f1343ccadc6c4e0c4d7
zen96k <jolly96k@gmail.com>
2025-10-19 03:05:56 +0900

    開発環境を調整

--- frontend/web/eslint.config.js
+++ frontend/web/eslint.config.js
@@ -1,10 +1,6 @@
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-export const config = withNuxt()
-  .append(eslintConfigPrettier)
-  .override("nuxt/vue/rules", {
-    rules: { "vue/static-class-names-order": "warn" }
-  })
+export const config = withNuxt().append(eslintConfigPrettier)
 
 export default config
