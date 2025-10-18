50cd1dcb3b3d26fe0401a374b34fa1e36bf42bc8
zen96k <jolly96k@gmail.com>
2025-10-19 00:26:02 +0900

    シードデータ作成処理を更新

--- backend/db/prisma/seed.ts
+++ backend/db/prisma/seed.ts
@@ -3,12 +3,14 @@ import { PrismaClient } from "../type/prisma/client.ts"
 const main = async () => {
   const dbClient = new PrismaClient()
 
-  await dbClient.rssPublisher.createMany({
-    data: [
-      { name: "Qiita", url: "https://qiita.com/popular-items/feed.atom" },
-      { name: "Zenn", url: "https://zenn.dev/feed" }
-    ],
-    skipDuplicates: true
+  await dbClient.$transaction(async (transaction) => {
+    await transaction.rssPublisher.createMany({
+      data: [
+        { name: "Qiita", url: "https://qiita.com/popular-items/feed.atom" },
+        { name: "Zenn", url: "https://zenn.dev/feed" }
+      ],
+      skipDuplicates: true
+    })
   })
 }
 
