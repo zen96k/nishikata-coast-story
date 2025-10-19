2ac5aec3ee39b3cca47c97785e1bdad51dced417
zen96k <jolly96k@gmail.com>
2025-10-20 01:46:51 +0900

    デプロイをテスト

--- backend/db/script/deploy.sh
+++ backend/db/script/deploy.sh
@@ -7,9 +7,4 @@ PROJECT_DIRNAME=$(cd ${SCRIPT_DIRNAME}/.. && pwd)
 
 cd ${PROJECT_DIRNAME}
 
-docker container run -it \
-                     --rm \
-                     -v $(pwd):/app \
-                     -v $(pwd)/.env.keys:/app/.env.keys \
-                     dotenv/dotenvx run -f compose.${NCS_ENV}.env -- \
-                     node --version
+dotenvx run -f compose.${NCS_ENV}.env -- docker compose -f compose.migrate.yml up -d --pull always --force-recreate -V --wait
