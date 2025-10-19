9a7228054e465acd7bd6cbe0e3c009f3a81ae14f
zen96k <jolly96k@gmail.com>
2025-10-19 18:30:46 +0900

    開発環境を調整

--- .devcontainer/script/on_create_command.sh
+++ .devcontainer/script/on_create_command.sh
@@ -4,6 +4,15 @@ set -euxo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
+apt update && apt full-upgrade -y
+
+apt update && apt install -y curl wget \
+                             git \
+                             zip unzip \
+                             rsync \
+                             tzdata
+ln -snf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
 apt update && apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended
