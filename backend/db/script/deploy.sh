ccf43acfee8bf20bd79191a08eea52446c1c053d
zen96k <jolly96k@gmail.com>
2025-10-20 04:54:39 +0900

    デプロイをテスト

--- backend/db/script/deploy.sh
+++ backend/db/script/deploy.sh
@@ -7,4 +7,6 @@ PROJECT_DIRNAME=$(cd ${SCRIPT_DIRNAME}/.. && pwd)
 
 cd ${PROJECT_DIRNAME}
 
+npm install && npm prune
 dotenvx run -f compose.${NCS_ENV}.env -- docker compose -f compose.migrate.yml up -d --pull always --force-recreate -V --wait
+dotenvx run -f compose.${NCS_ENV}.env -- PRISMA_DATABASE_URL=mysql://${MARIADB_USER}:${MARIADB_PASSWORD}@localhost:5307/${MARIADB_DATABASE} npm run prisma:migrate:deploy && npm run prisma:db:seed
