c81543a3ba138773bdf1616d8bf84884f1839b7f
zen96k <jolly96k@gmail.com>
2025-10-28 02:42:55 +0900

    開発環境を調整

--- backend/batch/eslint.config.js
+++ backend/batch/eslint.config.js
@@ -2,15 +2,10 @@ import eslint from "@eslint/js"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import { defineConfig } from "eslint/config"
 import tseslint from "typescript-eslint"
-import { fileURLToPath } from "url"
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = import.meta.dirname || path.dirname(filename)
 
 const config = defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  { languageOptions: { parserOptions: { tsconfigRootDir: dirname } } },
   eslintConfigPrettier
 )
 
--- frontend/web/eslint.config.js
+++ frontend/web/eslint.config.js
@@ -1,12 +1,6 @@
 import eslintConfigPrettier from "eslint-config-prettier/flat"
-import { fileURLToPath } from "url"
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = import.meta.dirname || path.dirname(filename)
-
-const config = withNuxt().append(eslintConfigPrettier, {
-  languageOptions: { parserOptions: { tsConfigRootDir: dirname } }
-})
+const config = withNuxt().append(eslintConfigPrettier)
 
 export default config
