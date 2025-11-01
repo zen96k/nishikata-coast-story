452c9fe085134f273cc2b9968c9162c484dd85c0
zen96k <jolly96k@gmail.com>
2025-11-02 07:45:23 +0900

    デプロイをテスト

--- frontend/web/server/api/article.get.ts
+++ frontend/web/server/api/article.get.ts
@@ -2,13 +2,27 @@ import type { H3Error } from "h3"
 import superjson from "superjson"
 
 export default defineEventHandler(async (event) => {
-  const { ncsApiBaseUrl: ncsApiBaseUrl } = useRuntimeConfig(event)
+  // const { ncsApiBaseUrl: ncsApiBaseUrl } = useRuntimeConfig(event)
+  console.log(event)
 
   try {
-    const articles = await $fetch<DeserializedArticle[]>(
-      `${ncsApiBaseUrl}/article`,
-      { parseResponse: superjson.parse }
-    )
+    // const articles = await $fetch<DeserializedArticle[]>(
+    //   `${ncsApiBaseUrl}/article`,
+    //   { parseResponse: superjson.parse }
+    // )
+    const articles = [
+      {
+        id: BigInt(1),
+        rssPublisherId: BigInt(1),
+        title: "test",
+        link: "https://qiita.com",
+        author: "test",
+        summary: "test",
+        publishedAt: new Date(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]
 
     return superjson.stringify(articles)
   } catch (error) {
