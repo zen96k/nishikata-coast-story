c2c714b77da3732167b27b9eef5fa77c1a216992
zen96k <jolly96k@gmail.com>
2025-11-02 01:43:04 +0900

    デプロイをテスト

--- script/deploy_fullstack_app.sh
+++ script/deploy_fullstack_app.sh
@@ -13,11 +13,35 @@ BACKEND_DIRNAME=${PROJECT_DIRNAME}/backend
 COMMON_DIRNAME=${PROJECT_DIRNAME}/common
 
 cd ${PROJECT_DIRNAME}
-git pull
 nvm install
+rm -rf node_modules
 npm install -g @dotenvx/dotenvx
+
+cd ${COMMON_DIRNAME}
+nvm install
+rm -rf node_modules
+
+cd ${FRONTEND_DIRNAME}/web
+nvm install
+rm -rf node_modules
+
+cd ${BACKEND_DIRNAME}/api
+nvm install
+rm -rf node_modules
+
+cd ${BACKEND_DIRNAME}/batch
+nvm install
+rm -rf node_modules
+npm run ncs:down
 
 cd ${BACKEND_DIRNAME}/db
 nvm install
-npm install && npm run prisma:generate
+rm -rf node_modules
+npm clean-install
+npm run prisma:generate
+npm run ncs:down
 npm run ncs:migrate
+npm run ncs:up
+
+cd ${BACKEND_DIRNAME}/batch
+npm run ncs:up
