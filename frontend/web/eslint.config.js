d1d98bd48637835d00aefeb22a4fcade8dd3403c
zen96k <jolly96k@gmail.com>
2025-11-03 04:30:01 +0900

    Nuxt Imageを断念

--- frontend/web/eslint.config.js
+++ frontend/web/eslint.config.js
@@ -1,6 +1,8 @@
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-const config = withNuxt().append(eslintConfigPrettier)
+const config = withNuxt()
+  .append(eslintConfigPrettier)
+  .append({ ignores: ["**/error.vue"] })
 
 export default config
