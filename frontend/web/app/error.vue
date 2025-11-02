5bfa3b71081383f057efe5b5bead5d58f8ee7938
zen96k <jolly96k@gmail.com>
2025-11-03 02:43:31 +0900

    エラー画面を追加

---
+++ frontend/web/app/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <UApp>
+    <UError :error="error" />
+  </UApp>
+</template>
+
+<script setup lang="ts">
+  import type { NuxtError } from "#app"
+
+  const props = defineProps<{ error: NuxtError }>()
+</script>
