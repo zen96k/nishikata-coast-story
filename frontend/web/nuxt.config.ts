5174f08f8bf6c76c4f500da6469947e90af7a714
zen96k <jolly96k@gmail.com>
2025-11-02 22:40:43 +0900

    デプロイをテスト

--- frontend/web/nuxt.config.ts
+++ frontend/web/nuxt.config.ts
@@ -2,7 +2,7 @@
 const config = defineNuxtConfig({
   devtools: { enabled: true },
   app: { head: { title: "西方コーストストーリー", htmlAttrs: { lang: "ja" } } },
-  runtimeConfig: { ncsApiBaseUrl: process.env.NUXT_NCS_API_BASE_URL || "" },
+  runtimeConfig: { ncsApiBaseUrl: process.env.NUXT_NCS_API_BASE_URL },
   typescript: { typeCheck: "build" },
   css: ["assets/css/main.css"],
   modules: ["@nuxt/eslint", "@nuxt/ui", "@compodium/nuxt", "@vueuse/nuxt"]
