1b00facc7a6b524e05798013880712605afa468c
zen96k <jolly96k@gmail.com>
2025-11-16 06:44:06 +0900

    動いてくれ

--- backend/batch/test/helper/reset-db.ts
+++ backend/batch/test/helper/reset-db.ts
@@ -2,9 +2,11 @@ import dbClient from "./db-client.ts"
 
 const resetDb = async () => {
   await dbClient.$transaction(async (transaction) => {
-    await transaction.rssPublisher.deleteMany()
     await transaction.article.deleteMany()
+    await transaction.articleLabel.deleteMany()
+    await transaction.articleLabelRelation.deleteMany()
     await transaction.cronTaskSchedule.deleteMany()
+    await transaction.rssPublisher.deleteMany()
   })
 }
 
