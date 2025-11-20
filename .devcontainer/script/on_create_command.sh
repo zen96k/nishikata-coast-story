d956fec14f2d6e4d4089c486ec6143566d07f454
zen96k <jolly96k@gmail.com>
2025-11-21 08:02:05 +0900

    開発環境を調整

--- .devcontainer/script/on_create_command.sh
+++ .devcontainer/script/on_create_command.sh
@@ -25,6 +25,12 @@ sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended
 cp -rfv .zshrc ${HOME}
 npm completion >> ${HOME}/.zshrc
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t /usr/local/bin/
+rm -rf lazygit.tar.gz lazygit
+
 wget https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-amd64.deb
 apt install -y ./fastfetch-linux-amd64.deb
 rm -rf fastfetch-linux-amd64.deb
