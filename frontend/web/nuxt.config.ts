09ebb3118e29f975b38f828c482ab42520861ba6
zen96k <jolly96k@gmail.com>
2025-12-07 21:06:47 +0900

    OGP画像を生成

--- frontend/web/nuxt.config.ts
+++ frontend/web/nuxt.config.ts
@@ -2,13 +2,23 @@
 const config = defineNuxtConfig({
   devtools: { enabled: true },
   app: { head: { title: "西方コーストストーリー", htmlAttrs: { lang: "ja" } } },
+  site: {
+    url: "https://nishikata-coast-story.vercel.app",
+    name: "西方コーストストーリー"
+  },
   runtimeConfig: {
     private: { ncsApiBaseUrl: process.env.NUXT_NCS_API_BASE_URL },
     public: { ogpScannerApiBaseUrl: process.env.OGP_SCANNER_API_BASE_URL }
   },
   typescript: { typeCheck: true },
   css: ["assets/css/main.css"],
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/image", "@vueuse/nuxt"]
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/image",
+    "nuxt-og-image",
+    "@vueuse/nuxt"
+  ]
 })
 
 export default config
