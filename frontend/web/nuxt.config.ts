4f15feaaaa10499296d49de5399f7f462dbf07b6
zen96k <jolly96k@gmail.com>
2025-12-07 21:51:43 +0900

    OGP画像を指定したい

--- frontend/web/nuxt.config.ts
+++ frontend/web/nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 const config = defineNuxtConfig({
   devtools: { enabled: true },
-  app: { head: { title: "西方コーストストーリー", htmlAttrs: { lang: "ja" } } },
-  site: {
-    url: "https://nishikata-coast-story.vercel.app",
-    name: "西方コーストストーリー"
+  app: {
+    head: {
+      title: "西方コーストストーリー",
+      htmlAttrs: { lang: "ja" },
+      meta: [
+        { name: "description", content: "技術記事をまとめる学習用アプリ" },
+        {
+          property: "og:image",
+          content: "https://nuxt.com/assets/design-kit/logo-green-white.png"
+        }
+      ]
+    }
   },
   runtimeConfig: {
     private: { ncsApiBaseUrl: process.env.NUXT_NCS_API_BASE_URL },
@@ -12,13 +20,7 @@ const config = defineNuxtConfig({
   },
   typescript: { typeCheck: true },
   css: ["assets/css/main.css"],
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/ui",
-    "@nuxt/image",
-    "nuxt-og-image",
-    "@vueuse/nuxt"
-  ]
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/image", "@vueuse/nuxt"]
 })
 
 export default config
